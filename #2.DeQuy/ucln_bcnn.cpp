706f6dc7de18096fc29c7e5bb496127368fbd9b5
Thuc <vuthuc3152001@gmail.com>
2024-02-26 11:34:09 +0700

    add git ignore

--- #2.DeQuy/ucln_bcnn.cpp
+++ #2.DeQuy/ucln_bcnn.cpp
@@ -10,12 +10,16 @@ long long gcd(long long a, long long b)
     }
 }
 
-long long lcm (long long a, long long b){
+long long lcm(long long a, long long b)
+{
     return a / gcd(a, b) * b;
 }
 
 int main()
 {
-
+    long long a, b;
+    cin >> a >> b;
+    cout << gcd(a, b) << endl;
+    cout << lcm(a, b) << endl;
     return 0;
 }
